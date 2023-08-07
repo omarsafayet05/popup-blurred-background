19115499b6a6406232215131e06e19d5efbc76a9
omarsafayet05 <safayetkhan4431@gmail.com>
2023-08-07 15:51:53 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// function toggle() {
+//   var blur = document.getElementById("blur");
+//   blur.classList.toggle("active");
+// }
+
+function toggle() {
+  document.getElementById("blur").classList.toggle("active");
+  document.getElementById("popup").classList.toggle("active");
+}
